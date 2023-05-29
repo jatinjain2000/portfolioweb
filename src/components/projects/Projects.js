66a3286f579a3dafae3d4db022cfa7461d100b93
jatinjain2000 <67114483+jatinjain2000@users.noreply.github.com>
2023-05-29 23:57:12 +0530

    Update Projects.js

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -15,7 +15,7 @@ const Projects = () => {
     >
       <div className="flex justify-center items-center text-center">
         <Title
-          title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
+          title="VISIT MY PORTFOLIO"
           des="My Projects"
         />
       </div>
