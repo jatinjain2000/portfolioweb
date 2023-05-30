82148d89ca7463b371222ccacb609ae81d76edb9
jatinjain2000 <67114483+jatinjain2000@users.noreply.github.com>
2023-05-30 09:46:23 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,14 @@
 import toconvert from "./images/projects/toconvert.jpg";
 import newsmokey from "./images/projects/newsmokey.jpg"
 import eyeimage from "./images/projects/eyeimage.jpg"
-import cv_new from "./images/projects/cv_new.pdf"
+import RESUMENEW from "./images/projects/RESUMENEW.pdf"
 import con from "./images/projects/con.png"
+import cv_new from "./images/projects/RESUMENEW.pdf"
 export{
     toconvert,
     newsmokey,
     eyeimage,
     cv_new,
     con,
+    RESUMENEW
 }
